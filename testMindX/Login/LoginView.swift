267e398d624d38037d57e81f839ff467818119aa
VinhLN <vinhln@rikkeisoft.com>
2022-10-14 09:42:31 +0700

    first

---
+++ testMindX/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  testMindX
+//
+//  Created by VinhLN on 13/10/2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ testMindX/Login/Register.swift
@@ -0,0 +1,20 @@
+//
+//  Register.swift
+//  testMindX
+//
+//  Created by DongNH on 13/10/2022.
+//
+
+import SwiftUI
+
+struct Register: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Register_Previews: PreviewProvider {
+    static var previews: some View {
+        Register()
+    }
+}
