8a112b401f0637a782e81829a6f6a32d2166a6c8
VinhLN <vinhln@rikkeisoft.com>
2022-10-18 14:18:24 +0700

    add Realm

---
+++ testMindX/Common/ButtonLogin.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonLogin.swift
+//  testMindX
+//
+//  Created by VinhLN on 18/10/2022.
+//
+
+import SwiftUI
+
+struct ButtonLogin: View {
+    @Binding var disabled: Bool
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonLogin_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonLogin()
+    }
+}
--- testMindX/Login/LoginView.swift
+++ testMindX/Login/LoginView.swift
@@ -6,18 +6,20 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct LoginView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var isForgot: Bool = false
+    @State private var isNext: Bool = false
     
 	// MARK: - Body
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             header
             inputView
-            
+            signInButton
         }
         .padding(.horizontal, 16)
     }
@@ -37,18 +39,33 @@ extension LoginView {
     }
     
     var inputView: some View {
-        VStack(spacing: 5) {
+        VStack(spacing: 10) {
             TextFieldCommon(image: "ic_mail", title: "", placeholder: "Email", text: $email)
             SecureFieldCommon(image: "ic_lock", title: "", placeholder: "Password", text: $password)
+            
             Button {
                 isForgot.toggle()
             } label: {
                 Text("Forgot password?")
+                    .foregroundColor(Color.black.opacity(0.8))
+                    .font(.system(size: 12))
             }
-
+            .frame(maxWidth: .infinity, alignment: .trailing)
         }
     }
     
+    var signInButton: some View {
+        Button {
+            isNext.toggle()
+        } label: {
+            Text("SIGN IN")
+                .foregroundColor(Color.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: 40)
+        .background(Color.black)
+        .cornerRadius(5)
+    }
+
 }
 
 struct LoginView_Previews: PreviewProvider {
